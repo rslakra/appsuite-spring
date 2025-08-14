20419de8af8a795d14ebe6f66cd52b7f63d45769
Rohtash Lakra <rslakra@users.noreply.github.com>
2025-08-13 19:24:53 -0700

    added parser test

---
+++ src/test/java/com/rslakra/appsuite/spring/parser/ParserTest.java
@@ -0,0 +1,18 @@
+package com.rslakra.appsuite.spring.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Rohtash Lakra
+ * @created 2/6/24 4:01 PM
+ */
+public class ParserTest {
+    
+    @Test
+    public void testParser() {
+        assertTrue(true);
+    }
+    
+}
